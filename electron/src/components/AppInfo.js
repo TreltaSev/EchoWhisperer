86b791996164fda1511a2bca2df0079e04394729
Trelta <treltasev@gmail.com>
2023-07-15 13:48:34 -0400

    Finished getCurrentTime method in AppInfo.js

--- electron/src/components/AppInfo.js
+++ electron/src/components/AppInfo.js
@@ -18,7 +18,21 @@ const BTextGroup = (props) => {
     )
 }
 
-{/* Current Time Component */}
+
+
+{/* Current Time Method */}
+const getCurrentTime = (props) => {
+    const _date = new Date();
+    const _hours = _date.getHours();
+    const _minutes = _date.getMinutes();
+    const ampm = _hours >= 12 ? "pm" : "am";
+    _hours = _hours % 12;
+    _hours = _hours ? _hours : 12;
+    _hours = String(_hours).padStart(2, "0");
+    _minutes = String (_minutes).padStart(2, "0");
+    return `${_hours}:${_minutes} ${ampm}`;
+}
+
 
 {/* App Info Component */}
 const AppInfo = () => {
