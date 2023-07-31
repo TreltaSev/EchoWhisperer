4ba0d81759b13918e98f276b94739f1bc90c993a
TreltaSev <treltasev@gmail.com>
2023-07-31 04:10:54 -0400

    Worked on init method

--- rust/src/server.rs
+++ rust/src/server.rs
@@ -16,15 +16,27 @@ use std::error::Error;
  impl Connection {
 
     // Handle Connections
-    async fn handle(stream: tokio::net::TcpStream) {
+    async fn handle(&mut self, stream: tokio::net::TcpStream) {
         if let Err(e) = None {}
     }
 
     // Process Connections
-    async fn process(stream: tokio::net::TcpStream) -> Result<(), Box<dyn Error>> {
+    async fn process(&mut self, stream: tokio::net::TcpStream) -> Result<(), Box<dyn Error>> {
         let websocket_stream = accept_async(stream).await.expect("[~] Error During Websocket Process");
         println!("Connected");
         return Ok(());
     }
 
+    // Initialize Everything
+    #[tokio::main]
+    async fn init(&mut self) {
+        let ip_address = "127.0.0.1:2492";
+        let tcp_listener = TcpListener::bind(&ip_address).await.expect("Failed to bind listener with address");
+
+        println!("Websocket running on: {}", ip_address);
+
+        while let Ok((stream, _)) = listener.accept().await {
+            tokio::spawn(self.handle(stream));
+        }
+    }
  }
