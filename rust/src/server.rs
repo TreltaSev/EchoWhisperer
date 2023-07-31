0fd6d0a96ab60d442e248e889eda159162def61e
TreltaSev <treltasev@gmail.com>
2023-07-31 04:07:03 -0400

    Added Process Method

--- rust/src/server.rs
+++ rust/src/server.rs
@@ -7,6 +7,7 @@
 use tokio::net::TcpListener;
 use tokio_tungstenite::tungstenite::protocol::Message;
 use tokio_tungstenite::accept_async;
+use std::error::Error;
 
 // Initialize Connection Implementation
  pub struct Connection;
@@ -18,4 +19,12 @@ use tokio_tungstenite::accept_async;
     async fn handle(stream: tokio::net::TcpStream) {
         if let Err(e) = None {}
     }
+
+    // Process Connections
+    async fn process(stream: tokio::net::TcpStream) -> Result<(), Box<dyn Error>> {
+        let websocket_stream = accept_async(stream).await.expect("[~] Error During Websocket Process");
+        println!("Connected");
+        return Ok(());
+    }
+
  }
